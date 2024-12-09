2f277645ef1988802b68d6f1547dbe8c02d15bc7
SJcodeML <sidrajabin56@gmail.com>
2024-12-09 17:38:23 +0500

    code added

--- hackathon-ecommerce-template/my-app/src/app/component/LatestProducts.tsx
+++ hackathon-ecommerce-template/my-app/src/app/component/LatestProducts.tsx
@@ -1,5 +1,12 @@
 "use client"
 import Image , {StaticImageData} from "next/image";
+
+import chair from "../../public/chair.png";
+import chair2 from "../../public/chair2.png";
+import chair3 from "../../public/chair3.png";
+import chair4 from "../../public/chair4.png";
+import chair5 from "../../public/chair5.png";
+import chair6 from "../../public/chair6.png";
 import chair7 from "../../public/chair7.png";
 
 interface Data2 {
@@ -13,7 +20,7 @@ interface Data2 {
 const data:Data2[] =[
     {
         id:1,
-        image:chair7,
+        image:chair,
         name:"sidra",
         price:42.00,
         oldPrice:65.00
@@ -21,7 +28,7 @@ const data:Data2[] =[
     },
     {
         id:2,
-        image:chair7,
+        image:chair2,
         name:"sidra",
         price:42.00,
         oldPrice:65.00
@@ -29,7 +36,7 @@ const data:Data2[] =[
     },
     {
         id:3,
-        image:chair7,
+        image:chair3,
         name:"sidra",
         price:42.00,
         oldPrice:65.00
@@ -37,7 +44,7 @@ const data:Data2[] =[
     },
     {
         id:4,
-        image:chair7,
+        image:chair4,
         name:"sidra",
         price:42.00,
         oldPrice:65.00
@@ -45,7 +52,7 @@ const data:Data2[] =[
     },
     {
         id:5,
-        image:chair7,
+        image:chair5,
         name:"sidra",
         price:42.00,
         oldPrice:65.00
@@ -53,7 +60,7 @@ const data:Data2[] =[
     },
     {
         id:6,
-        image:chair7,
+        image:chair6,
         name:"sidra",
         price:42.00,
         oldPrice:65.00
@@ -62,57 +69,125 @@ const data:Data2[] =[
 
 ]
 
+// const Card = ({ item }: { item: Data2 }) => (  
+//     <div className=" mt-5   ">
+        
+        {/* card container */}
+        {/* <div className="w-96 flex justify-between items-center flex-col  flex-wrap  mb-8 "> */}
+              
+              {/* card */}
+              {/* <div className='flex flex-col w-full sm:w-1/2 lg:w-1/3  ' key={item.id}>  
+                  <div className='w-60 h-40 bg-[#f7f7f7] flex justify-center items-center drop-shadow-xl'>  
+                      <Image  
+                          src={item.image}  
+                          alt="Picture of the author"  
+                          width={150}  
+                          height={150}  
+                      />  
+                  </div>  
+                  <div className='w-60 h-8 bg-white flex justify-evenly items-center drop-shadow-xl mb-12 hover:bg-blue-500'>  
+                      <h3 className='text-[#151875] text-[0.75rem]'>{`Code ${item.id}`}</h3>  
+                      <div className="flex ">
+                      <h3 className='text-[#151875] text-[0.75rem] gap-2'>${item.price.toFixed(2)}</h3>  
+                      <h3 className='text-[#ba3131] text-[0.75rem] line-through'>${item.oldPrice.toFixed(2)}</h3> 
+                      </div> 
+                  </div>  
+              </div> 
+        </div>
+    </div> 
+);   */}
+
 const Card = ({ item }: { item: Data2 }) => (  
-    <div className="flex justify-center items-center flex-row flex-wrap mt-5 mx-1  ">
-        {/* card */}
-    <div className='flex flex-col w-full sm:w-1/2 lg:w-1/3 px-4 mb-8  ' key={item.id}>  
-        <div className='w-60 h-40 bg-[#f7f7f7] flex justify-center items-center drop-shadow-xl'>  
+    <div className="w-full max-w-xs bg-white shadow-md  mb-4">  
+        {/* Image Container */}  
+        <div className="flex justify-center items-center bg-[#f7f7f7] h-80 overflow-hidden">  
             <Image  
                 src={item.image}  
-                alt="Picture of the author"  
+                alt={`Image of ${item.name}`} // Use a more descriptive alt text if relevant  
+                layout="responsive"  
                 width={150}  
                 height={150}  
+                className="object-cover"  
             />  
         </div>  
-        <div className='w-60 h-8 bg-white flex justify-evenly items-center drop-shadow-xl mb-12 hover:bg-blue-500'>  
-            <h3 className='text-[#151875] text-[0.75rem]'>{`Code ${item.id}`}</h3>  
-            <div className="flex ">
-            <h3 className='text-[#151875] text-[0.75rem] gap-2'>${item.price.toFixed(2)}</h3>  
-            <h3 className='text-[#ba3131] text-[0.75rem] line-through'>${item.oldPrice.toFixed(2)}</h3> 
-            </div> 
+
+        {/* Card Details */}  
+        <div className="p-4 flex flex-col">  
+            <h3 className="text-[#151875] text-[1rem]">{`Code ${item.id}`}</h3>  
+            <div className="flex items-end justify-between ">  
+                <span className="text-[#151875] text-[0.75rem]">${item.price.toFixed(2)}</span>  
+                {item.oldPrice && (  
+                    <span className="text-[#ba3131] text-[0.75rem] line-through">${item.oldPrice.toFixed(2)}</span>  
+                )}  
+            </div>  
         </div>  
-    </div> 
-    </div> 
-);  
+    </div>  
+);
 
-
-
-
-export default function LatestProducts(){
-    return(
-        <div className=" flex flex-wrap justify-center items-center">
-             {/* heading */}
-               <h1 className="">
-                Latest Products
-               </h1>
+// export default function LatestProducts(){
+//     return(
+//         <div className=" flex flex-wrap justify-center items-center">
+//              {/* heading */}
+//                <h1 className="">
+//                 Latest Products
+//                </h1>
              
-             {/* cards section */}
-             <div className='flex flex-wrap justify-between '>  
-            {data.map((item) => (  
-                <Card item={item} key={item.id} />  
-            ))}  
-        </div>  
+//              {/* cards section */}
+//              <div className='flex flex-wrap justify-between '>  
+//             {data.map((item) => (  
+//                 <Card item={item} key={item.id} />  
+//             ))}  
+//         </div>  
 
 
 
 
-        </div>
+//         </div>
         
-    )
+//     )
+// }
+
+
+
+export default function LatestProducts() {  
+    return (  
+        <div className="flex flex-col items-center mt-10">  
+            {/* Heading */}  
+            <div className="flex justify-center items-center">  
+                <h1 className="navbar-text text-[1.75rem] leading-7 mt-10 font-extrabold text-[#1a0b5b] hoverEffect">  
+                    Latest Products 
+                </h1>  
+            </div> 
+
+            {/* bottom nav featured products */}
+
+            <div className="flex justify-center gap-7 items-center mb-7 mt-3  ">  
+                <h1 className="navbar-text text-[0.875rem] leading-7 cursor-pointer hover:text-pink-300 text-[#1a0b5b] hoverEffect">  
+                    New Arrival 
+                </h1> 
+                <h1 className="navbar-text text-[0.875rem] leading-7  cursor-pointer hover:text-pink-300 text-[#1a0b5b] hoverEffect">  
+                    Best Seller 
+                </h1>  
+                <h1 className="navbar-text text-[0.875rem] leading-7  cursor-pointer hover:text-pink-300 text-[#1a0b5b] hoverEffect">  
+                    Featured 
+                </h1>  
+                <h1 className="navbar-text text-[0.875rem] leading-7  cursor-pointer hover:text-pink-300 text-[#1a0b5b] hoverEffect">  
+                    Best Offer 
+                </h1>   
+            </div> 
+
+
+
+
+            {/* Cards Section */}  
+            <div className="flex flex-wrap justify-center md:justify-center gap-4 px-4">  
+                {data.map((item) => (  
+                    <Card item={item} key={item.id} />  
+                ))}  
+            </div>  
+        </div>  
+    );  
 }
-
-
-
 
 
 
--- hackathon-ecommerce-template/my-app/src/app/component/ShopexOffer.tsx
+++ hackathon-ecommerce-template/my-app/src/app/component/ShopexOffer.tsx
@@ -1,5 +1,5 @@
 
-import Image, {StaticImageData} from "next/image";
+import Image, { StaticImageData } from "next/image";
 import freedelivery1 from "../../public/free-delivery1.png";
 import freedelivery2 from "../../public/Group.png";
 import freedelivery3 from "../../public/Group1.png";
@@ -7,78 +7,86 @@ import freedelivery4 from "../../public/cashback1.png";
 
 
 interface Data2 {
-    id:number,
-    image:StaticImageData,
-    name:string,
-    description:string
-   
+    id: number,
+    image: StaticImageData,
+    name: string,
+    description: string
+
 }
 
-const data:Data2[] =[
+const data: Data2[] = [
     {
-        id:1,
-        image:freedelivery1,
-        name:"24/7 Support",
-        description:"cnjdkj jkscjkhjdhf nscnsdjkkhsdjf jndcjnjskdh cndnjhdjfhdj njcsdjkfhjh njsdjfhjd njdjkhfjfh",
+        id: 1,
+        image: freedelivery1,
+        name: "24/7 Support",
+        description: "cnjdkj jkscjkhjdhf nscnsdjkkhsdjf jndcjnjskdh cndnjhdjfhdj njcsdjkfhjh njsdjfhjd njdjkhfjfh",
     },
     {
-        id:2,
-        image:freedelivery2,
-        name:"24/7 Support",
-        description:"cnjdkj jkscjkhjdhf nscnsdjkkhsdjf jndcjnjskdh cndnjhdjfhdj njcsdjkfhjh njsdjfhjd njdjkhfjfh",
+        id: 2,
+        image: freedelivery2,
+        name: "24/7 Support",
+        description: "cnjdkj jkscjkhjdhf nscnsdjkkhsdjf jndcjnjskdh cndnjhdjfhdj njcsdjkfhjh njsdjfhjd njdjkhfjfh",
     },
     {
-        id:3,
-        image:freedelivery3,
-        name:"24/7 Support",
-        description:"cnjdkj jkscjkhjdhf nscnsdjkkhsdjf jndcjnjskdh cndnjhdjfhdj njcsdjkfhjh njsdjfhjd njdjkhfjfh",
+        id: 3,
+        image: freedelivery3,
+        name: "24/7 Support",
+        description: "cnjdkj jkscjkhjdhf nscnsdjkkhsdjf jndcjnjskdh cndnjhdjfhdj njcsdjkfhjh njsdjfhjd njdjkhfjfh",
     },
     {
-        id:4,
-        image:freedelivery4,
-        name:"24/7 Support",
-        description:"cnjdkj jkscjkhjdhf nscnsdjkkhsdjf jndcjnjskdh cndnjhdjfhdj njcsdjkfhjh njsdjfhjd njdjkhfjfh",
+        id: 4,
+        image: freedelivery4,
+        name: "24/7 Support",
+        description: "cnjdkj jkscjkhjdhf nscnsdjkkhsdjf jndcjnjskdh cndnjhdjfhdj njcsdjkfhjh njsdjfhjd njdjkhfjfh",
     },
-   
+
 
 ]
 
-const Card = ({ item }: { item: Data2 }) => (  
+const Card = ({ item }: { item: Data2 }) => (
     <div className="flex  flex-wrap mt-5 px-32 ">
+        
+
+
         {/* card */}
         <div className='flex drop-shadow-xl ' key={item.id}>
-                             <div className='w-60 h-56 flex flex-col bg-[#fbfafa] justify-center items-center  mb-14 '>
-                                <Image
-                                    src={item.image}
-                                    alt="Picture of the author"
-                                    width={50}
-                                    height={50}
-                                />
-                            
-                            
-                                <h3 className='text-[#151875] text-[1rem] leading-6 font-extrabold navbar-text '>{`Code ${item.name}`}</h3>
-                                <h3 className='text-[0.75rem] text-center m-3 font-semibold text-[#8a8fb9]'>{item.description}</h3>
-                            </div>
-                            
-                               
-                            </div>
-                            </div>
-                        
-);  
+            <div className='w-60 h-56 flex flex-col bg-[#fbfafa] justify-center items-center  mb-14 '>
+                <Image
+                    src={item.image}
+                    alt="Picture of the author"
+                    width={50}
+                    height={50}
+                />
 
 
-export default function ShopexOffer (){
+                <h3 className='text-[#151875] text-[1rem] leading-6 font-extrabold navbar-text '>{`Code ${item.name}`}</h3>
+                <h3 className='text-[0.75rem] text-center m-3 font-semibold text-[#8a8fb9]'>{item.description}</h3>
+            </div>
+
+
+        </div>
+    </div>
+
+);
+
+
+export default function ShopexOffer() {
     return (
         <div className='flex flex-col '>
-           
-              {/* cards section */}
-              <div className='flex flex-wrap justify-between '>  
-            {data.map((item) => (  
-                <Card item={item} key={item.id} />  
-            ))}  
-        </div>  
+            <div className="flex justify-center items-center mt-10 mb-7">
+            <h1 className="navbar-text text-[1.75rem] leading-7 mt-10 font-extrabold text-[#1a0b5b] hoverEffect">
+                Latest Products
+            </h1>
         </div>
-                      
+
+            {/* cards section */}
+            <div className='flex flex-wrap justify-between '>
+                {data.map((item) => (
+                    <Card item={item} key={item.id} />
+                ))}
+            </div>
+        </div>
+
     )
 }
 
